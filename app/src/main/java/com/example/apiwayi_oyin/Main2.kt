5102e2ddd6d2a5f83f6af58340966ca6cae665a4
Berdiyar <github.com>
2020-03-31 11:07:57 +0500

    birinshi saqlaw jeri

--- app/src/main/java/com/example/apiwayi_oyin/Main2.kt
+++ app/src/main/java/com/example/apiwayi_oyin/Main2.kt
@@ -13,7 +13,9 @@ import kotlinx.android.synthetic.main.activity_main2.*
 class Main2 : AppCompatActivity() {
 
     private val random = Random()
-    private var durisjuwap: Int = 0
+    private var durisjuwap2: Float = 0F
+    private var aylanis: Int = 0
+    private val max_soraw_sani = 15
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +26,16 @@ class Main2 : AppCompatActivity() {
     }
 
     fun basilganda(view: View){
-        val basil = (view as Button).toString().toInt()
-        if(basil==durisjuwap){
+        val basil = (view as Button).text.toString().toFloat()
+        if(aylanis==max_soraw_sani){
+            val sms = Intent (this, Main3Activity::class.java)
+            startActivity(sms)
+            finish()
+        }
+        else if(basil==durisjuwap2){
             generatsiylaw()
+            aylanis+=1
+
         }
         else{
             val sms = Intent (this, Main3::class.java)
@@ -49,13 +58,20 @@ class Main2 : AppCompatActivity() {
             2->soraw.text = "$bir * $eki"
             else->soraw.text = "$bir / $eki"
         }
-        durisjuwap = when(belgi){
-            0->bir + eki
-            1->bir - eki
-            2->bir * eki
-            else->bir / eki
+        when(belgi){
+            0->{
+                durisjuwap2 = bir.toFloat() + eki.toFloat()
+            }
+            1->{
+                durisjuwap2 = bir.toFloat() - eki.toFloat()
+            }
+            2->{
+                durisjuwap2 = bir.toFloat() * eki.toFloat()
+            }
+            else->{
+                durisjuwap2 = bir.toFloat() / eki.toFloat()
+            }
         }
-
         qate_juwap_generatsiyasi(juwap_1)
         qate_juwap_generatsiyasi(juwap_2)
         qate_juwap_generatsiyasi(juwap_3)
@@ -63,17 +79,17 @@ class Main2 : AppCompatActivity() {
 
         when(random.nextInt(4)){
             0->{
-                juwap_1.text = durisjuwap.toString()
+                juwap_1.text = durisjuwap2.toString()
             }
             1->{
-                juwap_2.text = durisjuwap.toString()
+                juwap_2.text = durisjuwap2.toString()
                 }
             2-> {
-                juwap_3.text = durisjuwap.toString()
+                juwap_3.text = durisjuwap2.toString()
 
             }
             else->{
-                juwap_4.text = durisjuwap.toString()
+                juwap_4.text = durisjuwap2.toString()
             }
         }
 
@@ -83,12 +99,16 @@ class Main2 : AppCompatActivity() {
     private fun qate_juwap_generatsiyasi(button: Button){
         when(random.nextBoolean()){
             true->{
-                button.text = (durisjuwap + random.nextInt(100)+1).toString()
+                button.text = (durisjuwap2 + random.nextInt(100).toFloat()+1).toString()
             }
             false->{
-                button.text = (durisjuwap - random.nextInt(100)+1).toString()
+                button.text = (durisjuwap2 - random.nextInt(100).toFloat()+1).toString()
             }
         }
     }
 
 }
+
+
+
+
